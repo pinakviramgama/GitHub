20cfc4d92a6baf9c5e26b848c2457fa2ee7a9036
PINAK V <pinakdv11@gmail.com>
2025-07-06 19:59:32 +0530

    commit

--- Fronted/src/components/auth/Login.jsx
+++ Fronted/src/components/auth/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/login", {
+      const res = await fetch("https://github-jdrf.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
